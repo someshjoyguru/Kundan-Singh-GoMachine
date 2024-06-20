ea1eaaa8ad2befb36ffc45e941831492bdbed8a4
Somesh Ghosh <someshghoshjoyguru@gmail.com>
2024-06-20 15:04:43 +0530

    metadata added

--- client/src/data/carImages.jsx
+++ client/src/data/carImages.jsx
@@ -1,17 +1,7 @@
-import CarAudi from "../images/cars-big/audia1.jpg"
-import CarGolf from "../images/cars-big/golf6.jpg";
-import CarToyota from "../images/cars-big/toyotacamry.jpg";
-import CarBmw from "../images/cars-big/bmw320.jpg";
-import CarMercedes from "../images/cars-big/bmw320.jpg";
-import CarPassat from "../images/cars-big/bmw320.jpg";
+import jcb from '../images/banners/bg-1.png';
 
 const carImages = {
-    "Audi A1 S-Line": CarAudi,
-    "VW Golf 6": CarGolf,
-    "Toyota Camry": CarToyota,
-    "BMW 320 ModernLine": CarBmw,
-    "Mercedes-Benz GLK": CarMercedes,
-    "VW Passat CC": CarPassat
+    "JCB": jcb,
 };
 
 export default carImages;
